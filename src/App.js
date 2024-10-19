9fe339052a1e51c60a71fa734fdcb6d42bff63fb
Bui Minh Huy <bmhuy2505@gmail.com>
2024-10-19 12:11:26 +0700

    Finish

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ExShoe from "./ExShoe/ExShoe";
+
+function App() {
+    return (
+        <div id="App" className="">
+            <ExShoe />
+        </div>
+    );
+}
+
+export default App;
